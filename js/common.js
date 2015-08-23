a8c57c619745963a1dfbf1831c1c27e77bc9d994
zanagi <kintoki.douji@gmail.com>
2015-08-23 20:25:28 +0300

    js/common.js: toRadians function

--- js/common.js
+++ js/common.js
@@ -7,3 +7,12 @@ function shuffle(arr){
     for(var j, x, i = arr.length; i; j = Math.floor(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
     return arr;
 }
+
+/**
+ * Converts angle from degrees to radians
+ * @param angle {Number}
+ * @returns {Number} angle in radians
+ */
+function toRadians(angle) {
+	return angle * Math.PI / 180;
+}
